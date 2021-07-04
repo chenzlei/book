f5389498fc0db80494131b48617359f44ffbc090
Chenzhilei <1304308574@qq.com>
2021-07-04 19:41:54 +0800

    添加测试

---
+++ src/test/java/com/example/book/test/HelloTest.java
@@ -0,0 +1,14 @@
+package com.example.book.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author chenzhilei
+ * @date 2021/7/4) 19:40)
+ */
+public class HelloTest {
+    @Test
+    void hello(){
+        System.out.println("hello");
+    }
+}
